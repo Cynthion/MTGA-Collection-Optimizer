49ee68a33eee5d14f59c7a52832cd799e5bf5816
Christian Lüthold <biocodr@gmail.com>
2019-01-31 14:24:56 +0100

    hooked up .net core exe with electron process

--- src/MtgaDeckBuilder.App/main.js
+++ src/MtgaDeckBuilder.App/main.js
@@ -37,7 +37,7 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', startApi) // replaced createWindow
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -57,3 +57,33 @@ app.on('activate', function () {
 })
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+const os = require('os');
+var apiProcess = null;
+
+function startApi() {
+  var proc = require('child_process').spawn;
+  //  run server
+  var apipath = path.join(__dirname, '..\\MtgaDeckBuilder.Api\\bin\\dist\\win\\MtgaDeckBuilder.Api.exe')
+//   if (os.platform() === 'darwin') {
+//     apipath = path.join(__dirname, '..//api//bin//dist//osx//Api')
+//   }
+  apiProcess = proc(apipath)
+
+  apiProcess.stdout.on('data', (data) => {
+    writeLog(`stdout: ${data}`);
+    if (mainWindow == null) {
+      createWindow();
+    }
+  });
+}
+
+//Kill process when electron exits
+process.on('exit', function () {
+  writeLog('exit');
+  apiProcess.kill();
+});
+
+function writeLog(msg){
+  console.log(msg);
+}

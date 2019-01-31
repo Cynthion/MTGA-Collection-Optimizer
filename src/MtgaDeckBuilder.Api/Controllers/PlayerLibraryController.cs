c00624630d96eaecfdd918c421add2d14bc59252
Christian Lüthold <biocodr@gmail.com>
2019-01-31 13:45:46 +0100

    setup self-hosting api

---
+++ src/MtgaDeckBuilder.Api/Controllers/PlayerLibraryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MtgaDeckBuilder.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class PlayerLibraryController : Controller
+    {
+        // GET api/playerlibrary
+        [HttpGet]
+        public IActionResult GetPlayerCollection()
+        {
+            var result = new[] {
+                new { FirstName = "John", LastName = "Doe" },
+                new { FirstName = "Chris", LastName = "Smith" }
+            };
+
+            return Ok(result);
+        }
+    }
+}

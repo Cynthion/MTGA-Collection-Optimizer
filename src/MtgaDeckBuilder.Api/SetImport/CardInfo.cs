f04e3bcbe8e131dc45c81bf04adc4c98dd41db3d
Christian Lüthold <biocodr@gmail.com>
2019-02-02 12:21:09 +0100

    import all Set cards

---
+++ src/MtgaDeckBuilder.Api/SetImport/CardInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace MtgaDeckBuilder.Api.SetImport
+{
+    public class CardInfo
+    {
+        public int MultiverseId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Number { get; set; }
+
+        public string Rarity { get; set; }
+
+        public string Uuid { get; set; }
+    }
+}
--- src/MtgaDeckBuilder.Api/SetImport/SetLoader.cs
+++ src/MtgaDeckBuilder.Api/SetImport/SetLoader.cs
@@ -1,6 +1,10 @@
-﻿using MtgaDeckBuilder.Api.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using MtgaDeckBuilder.Api.Configuration;
 using System.IO;
 using System.Linq;
+using MtgaDeckBuilder.Api.SetImport.Model;
+using Newtonsoft.Json;
 using NLog;
 
 namespace MtgaDeckBuilder.Api.SetImport
@@ -25,10 +29,50 @@ namespace MtgaDeckBuilder.Api.SetImport
             var setJsonFilePaths = filePaths
                 .Where(p => p.EndsWith(".json"));
 
+            var allCardInfos = new List<CardInfo>();
+
             foreach (var jsonFilePath in setJsonFilePaths)
             {
                 Logger.Info($"Found {jsonFilePath}");
+                string setJson;
+
+                try
+                {
+                    // TODO make async
+                    setJson = File.ReadAllText(jsonFilePath);
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e, $"Could not read {jsonFilePath}");
+                    break;
+                }
+    
+                try
+                {
+                    File.ReadAllText(jsonFilePath);
+
+                    // TODO figure out if only certain properties can be read
+                    var set = JsonConvert.DeserializeObject<Set>(setJson);
+                    var setCards = set.Cards;
+                    var setCardInfos = setCards.Select(c => new CardInfo
+                    {
+                        MultiverseId = c.MultiverseId,
+                        Name = c.Name,
+                        Number = c.Number,
+                        Rarity = c.Rarity,
+                        Uuid = c.Uuid,
+                    });
+
+                    allCardInfos.AddRange(setCardInfos);
+                }
+                catch (Exception e)
+                {
+                    Logger.Warn($"{jsonFilePath} could not be loaded as it doesn't fit the importing model.");
+                    Logger.Error(e);
+                }
             }
+
+            Logger.Info($"Found { allCardInfos.Count } set cards.");
         }
     }
 }

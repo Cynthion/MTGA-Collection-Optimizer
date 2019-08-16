8184ebdf939fd3ee3b0e0cbb103d2fd1873c17de
Christian Lüthold <biocodr@gmail.com>
2019-08-16 18:52:54 +0200

    calc card worthiness

--- UI/src/app/util/calculations.ts
+++ UI/src/app/util/calculations.ts
@@ -7,16 +7,31 @@ const wcUncommonDropRate: number = 1 / 5;       // 0.20
 const wcRareDropRate: number = 1 / 10;          // 0.10, not accurate according to real drop rates
 const wcMythicRareDropRate: number = 1 / 24;    // 0.04
 
-function calcCardWorthiness(collectionCard: CollectionCardState, playerDecks: PlayerDeckState[]): number {
-    // const nrOfDecks = playerDecks.filter(d => _.some(d.cards, c => collectionCard.mtgaId === c.mtgaId && c.requiredCount <= c.missingCount);
+// sum of (progress with card * deck total worth) per deck where that card is missing
+function calcCardWorth(collectionCard: CollectionCardState, playerDecks: PlayerDeckState[]): number {
+    // get decks containing that card
+    const containingDecks = playerDecks.filter(d => _.some(d.cards, (c: CollectionCardState) => collectionCard.mtgaId === c.mtgaId));
 
-    const nrOfDecks = playerDecks.filter(d => _.some(d.cards, c => collectionCard.mtgaId === c.mtgaId));
+    // get decks that miss that card
+    let cardWorth = 0;
+    for (let i = 0; i < containingDecks.length; i++) {
+        const containingDeck = containingDecks[i];
 
+        const ownedCount = collectionCard.ownedCount;
+        const requiredCount = containingDeck.cards.find(c => c.mtgaId === collectionCard.mtgaId).requiredCount;
 
-    return 0;
+        if (requiredCount < ownedCount) {
+            const deckProgressWithCard = ((containingDeck.totalOwnedCards + 1) / containingDeck.totalDeckCards) * 100;
+            const deckTotalWorth = calcDeckTotalWorth(containingDeck);
+
+            cardWorth += (deckProgressWithCard * deckTotalWorth);
+        }
+    }
+
+    return cardWorth;
 }
 
-function calcDeckWorth(deckState: PlayerDeckState): number {
+function calcDeckTotalWorth(deckState: PlayerDeckState): number {
     const nrOfCards = deckState.cards.length;
 
     const weightCommon = deckState.cards.filter(c => c.rarity === Rarity.Common).length * wcCommonDropRate;
@@ -31,6 +46,5 @@ function calcDeckWorth(deckState: PlayerDeckState): number {
 }
 
 function calcDeckProgressInPercent(deckState: PlayerDeckState): number {
-    // TODO is this percent?
-    return deckState.totalOwnedCards / deckState.totalDeckCards;
+    return 
 }

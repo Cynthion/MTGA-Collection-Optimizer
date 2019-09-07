301e6084541ef7a855e52da171609eb788f880a4
Christian Lüthold <biocodr@gmail.com>
2019-09-07 11:41:16 +0200

    subscribt to SSE in app.component

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActionsSubject, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
@@ -6,17 +6,22 @@ import { AppConfig } from '../environments/environment';
 import { WindowService } from './providers/window.service';
 import { ElectronService } from './providers/electron.service';
 import { PlatformServiceProvider } from './providers/platform-service-provider';
+import { makeInternalApiUrl } from './util/http';
 import { OpenAboutDialogAction } from './about';
 import { OpenSettingsDialogAction } from './settings';
 import { AppState, RootState } from './app.state';
+import { LoadDataAction } from './layout/layout.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   state$: Observable<AppState>;
+
+  protected eventSource: EventSource;
+  protected isSseSubscribed: boolean;
 
   private windowService: WindowService;
   private isWindowMaximized: boolean;
@@ -41,6 +46,14 @@ export class AppComponent {
 
     this.windowService = this.platformServiceProvider.getWindowService();
     this.isWindowMaximized = this.windowService.isWindowMaximized();
+  }
+
+  ngOnInit() {
+    this.subscribeToServerSentEvents();
+  }
+
+  ngOnDestroy() {
+    this.unsubscribeFromServerSentEvents();
   }
 
   openSettingsDialog(): void {
@@ -71,5 +84,41 @@ export class AppComponent {
 
   get isMaximized(): boolean {
     return this.isWindowMaximized;
+  }
+
+  subscribeToServerSentEvents() {
+    if (!this.isSseSubscribed) {
+      this.eventSource = new EventSource(makeInternalApiUrl('sse-layout-data'));
+      this.eventSource.onopen = (evt) => this.onEventSourceOpen(evt);
+      this.eventSource.onmessage = (data) => this.onEventSourceMessage(data);
+      this.eventSource.onerror = (evt) => this.onEventSourceError(evt);
+
+      this.isSseSubscribed = true;
+    }
+  }
+
+  unsubscribeFromServerSentEvents(): void {
+    if (this.isSseSubscribed) {
+      this.eventSource.close();
+      this.isSseSubscribed = false;
+    }
+  }
+
+  protected onEventSourceOpen(event: Event) {
+    console.log('SSE connection established');
+  }
+
+  protected onEventSourceMessage(messageEvent: MessageEvent): void {
+    console.log('SSE connection message:', messageEvent.data);
+
+    this.actionsSubject.next(new LoadDataAction());
+  }
+
+  protected onEventSourceError(event: Event): void {
+    console.log('SSE connection error:', event);
+    if (event.eventPhase === this.eventSource.CLOSED) {
+      this.eventSource.close();
+      console.log('SSE connection closed.');
+    }
   }
 }
--- UI/src/app/layout/layout.component.ts
+++ UI/src/app/layout/layout.component.ts
@@ -1,67 +1,10 @@
-import { Component, ChangeDetectionStrategy, OnInit, OnDestroy } from '@angular/core';
-import { Store, ActionsSubject } from '@ngrx/store';
-
-import { makeInternalApiUrl } from '../util/http';
-
-import { State } from './layout.state';
-import { LoadDataAction } from './layout.actions';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LayoutComponent implements OnInit, OnDestroy {
+export class LayoutComponent {
 
-  protected eventSource: EventSource;
-  protected isSseSubscribed: boolean;
-
-  constructor(
-    private store: Store<State>,
-    private actionsSubject: ActionsSubject,
-  ) { }
-
-  ngOnInit() {
-    this.subscribeToServerSentEvents();
-  }
-
-  ngOnDestroy() {
-    this.unsubscribeFromServerSentEvents();
-  }
-
-  subscribeToServerSentEvents() {
-    if (!this.isSseSubscribed) {
-      this.eventSource = new EventSource(makeInternalApiUrl('sse-layout-data'));
-      this.eventSource.onopen = (evt) => this.onEventSourceOpen(evt);
-      this.eventSource.onmessage = (data) => this.onEventSourceMessage(data);
-      this.eventSource.onerror = (evt) => this.onEventSourceError(evt);
-
-      this.isSseSubscribed = true;
-    }
-  }
-
-  unsubscribeFromServerSentEvents(): void {
-    if (this.isSseSubscribed) {
-      this.eventSource.close();
-      this.isSseSubscribed = false;
-    }
-  }
-
-  protected onEventSourceOpen(event: Event) {
-    console.log('SSE connection established');
-  }
-
-  protected onEventSourceMessage(messageEvent: MessageEvent): void {
-    console.log('SSE connection message:', messageEvent.data);
-
-    this.actionsSubject.next(new LoadDataAction());
-  }
-
-  protected onEventSourceError(event: Event): void {
-    console.log('SSE connection error:', event);
-    if (event.eventPhase === this.eventSource.CLOSED) {
-      this.eventSource.close();
-      console.log('SSE connection closed.');
-    }
-  }
 }
